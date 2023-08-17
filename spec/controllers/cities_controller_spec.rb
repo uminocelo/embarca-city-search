27c4c330db57e63b64949dd3791f5ac42761481f
uminocelo <marcelo.mdl.dev@gmail.com>
2023-08-17 10:14:42 -0300

    update tests

--- spec/controllers/cities_controller_spec.rb
+++ spec/controllers/cities_controller_spec.rb
@@ -2,111 +2,118 @@
 
 require 'rails_helper'
 
+RSpec.shared_examples 'a successful response' do
+  it 'returns a successful response' do
+    expect(response).to have_http_status(:success)
+  end
+end
+
+RSpec.shared_examples 'a JSON representation of the city' do
+  let(:state) { State.create(name: 'Espirito Santo', abbreviation: 'ES') }
+  let(:city) { City.create(name: 'Vitoria', state: state) }
+
+  it 'returns the correct JSON response' do
+    get :show, params: { id: city.id }
+    expected_response = { 'id' => city.id, 'name' => city.name, 'state_id' => state.id }
+    parsed_response = JSON.parse(response.body).slice('id', 'name', 'state_id')
+    expect(parsed_response).to eq(expected_response)
+  end
+end
+
+RSpec.shared_examples 'a new city creation' do
+  let(:state) { State.create(name: 'Espirito Santo', abbreviation: 'ES') }
+
+  it 'creates a new city' do
+    expect { post :create, params: valid_attributes }.to change(City, :count).by(1)
+  end
+
+  it 'returns a JSON representation of the created city' do
+    post :create, params: valid_attributes
+    expect(response).to have_http_status(:created)
+    expect_json_response('Vitoria', 'state_id', state.id)
+  end
+end
+
+RSpec.shared_examples 'an unprocessable entity response' do |invalid_key|
+  it 'does not create a new city' do
+    expect { post :create, params: invalid_attributes }.not_to change(City, :count)
+  end
+
+  it 'returns a JSON representation of errors' do
+    post :create, params: invalid_attributes
+    expect(response).to have_http_status(:unprocessable_entity)
+
+    json_response = JSON.parse(response.body)
+    expect(json_response).to have_key(invalid_key)
+  end
+end
+
+RSpec.shared_examples 'an updated city' do |new_name|
+  it 'updates the city name' do
+    state = State.create(name: 'Espirito Santo', abbreviation: 'ES')
+    city = City.create(name: 'Vitoria', state: state)
+
+    patch :update, params: { id: city.id, city: { name: new_name, state_id: state.id } }
+    city.reload
+
+    expect(city.name).to eq(new_name)
+  end
+
+  it_behaves_like 'a successful response'
+end
+
+RSpec.shared_examples 'a destroyed city' do
+  it 'destroys the city' do
+    state = State.create(name: 'Espirito Santo', abbreviation: 'ES')
+    city = City.create(name: 'Vitoria', state: state)
+    expect { delete :destroy, params: { id: city.id } }.to change(City, :count).by(-1)
+    expect(response).to have_http_status(:no_content)
+  end
+end
+
 RSpec.describe CitiesController, type: :controller do
   before(:each) do
     City.destroy_all
     State.destroy_all
   end
 
-  let(:state) { State.create(name: 'Espirito Santo', abbreviation: 'ES') }
-  let!(:city) { City.create(name: 'Vitoria', state: state) }
-
   describe 'GET #show' do
-    it 'returns a successful response' do
-      get :show, params: { id: city.id }
-      expect(response).to have_http_status(:success)
-    end
-
-    it 'returns the correct city JSON' do
-      get :show, params: { id: city.id }
-      expected_response = { 'id' => city.id, 'name' => city.name, 'state_id' => city.state_id }
-      parsed_response = JSON.parse(response.body).slice('id', 'name', 'state_id')
-      expect(parsed_response).to eq(expected_response)
-    end
+    it_behaves_like 'a successful response'
+    it_behaves_like 'a JSON representation of the city'
   end
 
   describe 'POST #create' do
     context 'with valid attributes' do
-      let(:valid_attributes) { { city: { name: 'Guarapari', state_id: state.id } } }
+      let(:valid_attributes) { { city: { name: 'Vitoria', state_id: state.id } } }
 
-      it 'creates a new city' do
-        expect { post :create, params: valid_attributes }.to change(City, :count).by(1)
-      end
-
-      it 'returns a JSON representation of the created city' do
-        post :create, params: valid_attributes
-        expect(response).to have_http_status(:created)
-
-        json_response = JSON.parse(response.body)
-        expect(json_response['name']).to eq('Guarapari')
-        expect(json_response['state_id']).to eq(state.id)
-      end
+      it_behaves_like 'a new city creation'
     end
 
     context 'with invalid attributes' do
       let(:invalid_attributes) { { city: { name: '' } } }
 
-      it 'does not create a new city' do
-        expect { post :create, params: invalid_attributes }.not_to change(City, :count)
-      end
-
-      it 'returns a JSON representation of errors' do
-        post :create, params: invalid_attributes
-        expect(response).to have_http_status(:unprocessable_entity)
-
-        json_response = JSON.parse(response.body)
-        expect(json_response).to have_key('name')
-      end
+      it_behaves_like 'an unprocessable entity response', 'name'
     end
   end
 
   describe 'PATCH #update' do
     context 'with valid attributes' do
-      let(:valid_attributes) { { city: { name: 'Vitoria', state_id: state.id } } }
-
-      before do
-        patch :update, params: { id: city.id, city: { name: 'Vitória' } }
-        city.reload
-      end
-
-      it 'updates the city name' do
-        expect(city.name).to eq('Vitória')
-      end
-
-      it 'returns a JSON representation of the updated city' do
-        expect(response).to have_http_status(:success)
-        json_response = JSON.parse(response.body)
-        expect(json_response['name']).to eq('Vitória')
-        expect(json_response['state_id']).to eq(state.id)
-      end
+      it_behaves_like 'an updated city', 'Guarapari'
     end
 
     context 'with invalid attributes' do
-      it 'does not update the city' do
-        original_name = city.name
-        patch :update, params: { id: city.id, city: { name: '' } }
-        city.reload
-        expect(city.name).to eq(original_name)
-      end
-
-      it 'returns a JSON representation of errors' do
-        patch :update, params: { id: city.id, city: { name: '' } }
-        expect(response).to have_http_status(:unprocessable_entity)
-
-        json_response = JSON.parse(response.body)
-        expect(json_response).to have_key('name')
-      end
+      let(:invalid_attributes) { { city: { name: '' } } }
+      it_behaves_like 'an unprocessable entity response', 'name'
     end
   end
 
   describe 'DELETE #destroy' do
-    it 'destroys the city' do
-      expect { delete :destroy, params: { id: city.id } }.to change(City, :count).by(-1)
-    end
+    it_behaves_like 'a destroyed city'
+  end
 
-    it 'returns a no content response' do
-      delete :destroy, params: { id: city.id }
-      expect(response).to have_http_status(:no_content)
-    end
+  def expect_json_response(name, key, value)
+    json_response = JSON.parse(response.body)
+    expect(json_response['name']).to eq(name)
+    expect(json_response[key]).to eq(value)
   end
 end
--- spec/controllers/states_controller_spec.rb
+++ spec/controllers/states_controller_spec.rb
@@ -2,6 +2,115 @@
 
 require 'rails_helper'
 
+RSpec.shared_context 'with states data' do
+  let!(:state1) { State.create(name: 'Espirito Santo', abbreviation: 'ES') }
+  let!(:state2) { State.create(name: 'Amazonas', abbreviation: 'AM') }
+
+  let(:states_data) do
+    [
+      { 'id' => state1.id, 'name' => 'Espirito Santo', 'abbreviation' => 'ES' },
+      { 'id' => state2.id, 'name' => 'Amazonas', 'abbreviation' => 'AM' }
+    ]
+  end
+end
+
+RSpec.shared_examples 'a JSON representation of states' do
+  it 'returns JSON representation of all states' do
+    get :index
+    expected_response = states_data
+    parsed_response = JSON.parse(response.body).map { |item| item.slice('id', 'abbreviation', 'name') }
+    expect(parsed_response).to eq(expected_response)
+  end
+end
+
+RSpec.shared_examples 'a JSON representation of a state for show action' do
+  it 'returns the correct state JSON' do
+    get :show, params: { id: state1.id }
+    expected_response = { 'id' => state1.id, 'name' => state1.name, 'abbreviation' => state1.abbreviation }
+    parsed_response = JSON.parse(response.body).slice('id', 'name', 'abbreviation')
+    expect(parsed_response).to eq(expected_response)
+  end
+end
+
+RSpec.shared_examples 'a new state creation' do
+  it 'creates a new state' do
+    expect { post :create, params: valid_attributes }.to change(State, :count).by(1)
+  end
+
+  it 'returns a JSON representation of the created state' do
+    post :create, params: valid_attributes
+    expect(response).to have_http_status(:created)
+
+    json_response = JSON.parse(response.body)
+    expect(json_response['name']).to eq(valid_attributes[:state][:name])
+    expect(json_response['abbreviation']).to eq(valid_attributes[:state][:abbreviation])
+  end
+end
+
+RSpec.shared_examples 'an updated state' do
+  before do
+    patch :update, params: { id: state.id, state: valid_attributes[:state] }
+    state.reload
+  end
+
+  it 'updates the state' do
+    expect(state.name).to eq(valid_attributes[:state][:name])
+  end
+
+  it 'returns a JSON representation of the updated state' do
+    expect(response).to have_http_status(:success)
+
+    json_response = JSON.parse(response.body)
+    expect(json_response['name']).to eq(valid_attributes[:state][:name])
+  end
+end
+
+RSpec.shared_examples 'an unprocessable entity response for state' do |invalid_key|
+  it 'does not update the state' do
+    original_name = state.name
+    patch :update, params: { id: state.id, state: { name: '' } }
+    state.reload
+    expect(state.name).to eq(original_name)
+  end
+
+  it 'returns a JSON representation of errors' do
+    patch :update, params: { id: state.id, state: { name: '' } }
+    expect(response).to have_http_status(:unprocessable_entity)
+
+    json_response = JSON.parse(response.body)
+    expect(json_response).to have_key(invalid_key)
+  end
+end
+
+RSpec.shared_examples 'a destroyed state' do
+  it 'destroys the state' do
+    expect { delete :destroy, params: { id: state.id } }.to change(State, :count).by(-1)
+  end
+
+  it 'returns a no content response' do
+    delete :destroy, params: { id: state.id }
+    expect(response).to have_http_status(:no_content)
+  end
+end
+
+RSpec.shared_examples 'a #index for cities' do
+  it 'returns a successful response' do
+    get :index
+    expect(response).to have_http_status(:success)
+  end
+
+  it_behaves_like 'a JSON representation of states'
+end
+
+RSpec.shared_examples 'a #show for city' do
+  it 'returns a successful response' do
+    get :show, params: { id: state1.id }
+    expect(response).to have_http_status(:success)
+  end
+
+  it_behaves_like 'a JSON representation of a state for show action'
+end
+
 RSpec.describe StatesController, type: :controller do
   before(:each) do
     City.destroy_all
@@ -9,78 +118,20 @@ RSpec.describe StatesController, type: :controller do
   end
 
   describe 'GET #index' do
-    it 'returns a successful response' do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-
-    it 'returns JSON representation of all states' do
-      state1 = State.create(name: 'Espirito Santo', abbreviation: 'ES')
-      state2 = State.create(name: 'Amazonas', abbreviation: 'AM')
-
-      get :index
-      expected_response = [
-        { 'id' => state1.id, 'name' => 'Espirito Santo', 'abbreviation' => 'ES' },
-        { 'id' => state2.id, 'name' => 'Amazonas', 'abbreviation' => 'AM' }
-      ]
-      parsed_response = JSON.parse(response.body).map { |response| response.slice('id', 'name', 'abbreviation') }
-
-      expect(parsed_response).to eq(expected_response)
-    end
+    include_context 'with states data'
+    it_behaves_like 'a #index for cities'
   end
 
   describe 'GET #show' do
-    let(:state) { State.create(name: 'Espirito Santo', abbreviation: 'ES') }
-
-    it 'returns a successful response' do
-      get :show, params: { id: state.id }
-      expect(response).to have_http_status(:success)
-    end
-
-    it 'returns the correct state JSON' do
-      get :show, params: { id: state.id }
-      expected_response = { 'id' => state.id, 'name' => 'Espirito Santo', 'abbreviation' => 'ES' }
-      parsed_response = JSON.parse(response.body).slice('id', 'name', 'abbreviation')
-      expect(parsed_response).to eq(expected_response)
-    end
+    include_context 'with states data'
+    it_behaves_like 'a #show for city'
   end
 
   describe 'POST #create' do
     context 'with valid attributes' do
       let(:valid_attributes) { { state: { name: 'Amazonas', abbreviation: 'AM' } } }
 
-      it 'creates a new state' do
-        expect do
-          post :create, params: valid_attributes
-        end.to change(State, :count).by(1)
-      end
-
-      it 'returns a JSON representation of the created state' do
-        post :create, params: valid_attributes
-        expect(response).to have_http_status(:created)
-
-        json_response = JSON.parse(response.body)
-        expect(json_response['name']).to eq('Amazonas')
-        expect(json_response['abbreviation']).to eq('AM')
-      end
-    end
-
-    context 'with invalid attributes' do
-      let(:invalid_attributes) { { state: { name: '' } } }
-
-      it 'does not create a new state' do
-        expect do
-          post :create, params: invalid_attributes
-        end.not_to change(State, :count)
-      end
-
-      it 'returns a JSON representation of errors' do
-        post :create, params: invalid_attributes
-        expect(response).to have_http_status(:unprocessable_entity)
-
-        json_response = JSON.parse(response.body)
-        expect(json_response).to have_key('name')
-      end
+      it_behaves_like 'a new state creation'
     end
   end
 
@@ -90,51 +141,17 @@ RSpec.describe StatesController, type: :controller do
     context 'with valid attributes' do
       let(:valid_attributes) { { state: { name: 'Espírito Santo', abbreviation: 'ES' } } }
 
-      it 'updates the state' do
-        patch :update, params: { id: state.id, state: { name: 'Espírito Santo' } }
-        state.reload
-        expect(state.name).to eq('Espírito Santo')
-      end
-
-      it 'returns a JSON representation of the updated state' do
-        patch :update, params: { id: state.id, state: { name: 'Espírito Santo' } }
-        expect(response).to have_http_status(:success)
-
-        json_response = JSON.parse(response.body)
-        expect(json_response['name']).to eq('Espírito Santo')
-      end
+      it_behaves_like 'an updated state'
     end
 
     context 'with invalid attributes' do
-      it 'does not update the state' do
-        original_name = state.name
-        patch :update, params: { id: state.id, state: { name: '' } }
-        state.reload
-        expect(state.name).to eq(original_name)
-      end
-
-      it 'returns a JSON representation of errors' do
-        patch :update, params: { id: state.id, state: { name: '' } }
-        expect(response).to have_http_status(:unprocessable_entity)
-
-        json_response = JSON.parse(response.body)
-        expect(json_response).to have_key('name')
-      end
+      it_behaves_like 'an unprocessable entity response for state', 'name'
     end
   end
 
   describe 'DELETE #destroy' do
     let!(:state) { State.create(name: 'Espirito Santo', abbreviation: 'ES') }
 
-    it 'destroys the state' do
-      expect do
-        delete :destroy, params: { id: state.id }
-      end.to change(State, :count).by(-1)
-    end
-
-    it 'returns a no content response' do
-      delete :destroy, params: { id: state.id }
-      expect(response).to have_http_status(:no_content)
-    end
+    it_behaves_like 'a destroyed state'
   end
 end
